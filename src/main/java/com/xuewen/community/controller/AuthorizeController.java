effc407b14466963c54b29bc6c11cc1f16d9de38
zhangkaijian <zhangkaijian@sinosoft.com.cn>
2019-09-13 13:44:05 +0800

    修改登录问题

--- src/main/java/com/xuewen/community/controller/AuthorizeController.java
+++ src/main/java/com/xuewen/community/controller/AuthorizeController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -86,6 +87,15 @@ public class AuthorizeController {
             return "redirect:/";
         }
 
+    }
+
+    @GetMapping("/loginout")
+    public String loginOUt(HttpServletRequest request, Model model){
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null){
+            request.getSession().removeAttribute("user");
+        }
+        return "index";
     }
 
 
